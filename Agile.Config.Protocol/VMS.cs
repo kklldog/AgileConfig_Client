3a765b0783177db5b355ab6d6411780cc56abcda
kklldog <kklldog@qq.com>
2020-07-31 15:16:55 +0800

    添加项目文件。

---
+++ Agile.Config.Protocol/VMS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Agile.Config.Protocol
+{
+    public class ConfigItem
+    {
+        public string key { get; set; }
+
+        public string value { get; set; }
+
+        public string group { get; set; }
+    }
+
+    public class ActionConst
+    {
+        public const string Add = "add";
+        public const string Update = "update";
+        public const string Remove = "remove";
+        public const string Offline = "offline";
+        public const string Reload = "reload";
+    }
+
+    public class WebsocketAction
+    {
+        public string Action { get; set; }
+
+        public ConfigItem Item { get; set; }
+
+        public ConfigItem OldItem { get; set; }
+    }
+}
---
+++ AgileConfig.Client/AgileConfigSource.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Agile.Config.Client
+{
+    public static class AgileConfitBuilderExt
+    {
+        public static IConfigurationBuilder AddAgileConfig(
+            this IConfigurationBuilder builder,
+            IConfigClient client)
+        {
+            return builder.Add(new AgileConfigSource(client));
+        }
+    }
+
+    public class AgileConfigSource : IConfigurationSource
+    {
+        protected IConfigClient ConfigClient { get; }
+
+        public AgileConfigSource(IConfigClient client)
+        {
+            ConfigClient = client;
+        }
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new AgileConfigProvider(ConfigClient);
+        }
+    }
+}
