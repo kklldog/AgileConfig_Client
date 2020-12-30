73ae477ec0fc4d804fde3b8004793cf2711f7de4
kklldog <kklldog@qq.com>
2020-12-30 20:27:06 +0800

    update mvc sample

--- AgileConfigMVCSample/Program.cs
+++ AgileConfigMVCSample/Program.cs
@@ -25,46 +25,23 @@ namespace AgileConfigMVCSample
             Host.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration((context, config) =>
             {
-                //读取本地配置
-                var localconfig = new ConfigurationBuilder()
-                                 .SetBasePath(Directory.GetCurrentDirectory())
-                                 .AddJsonFile("appsettings.json").Build();
-                //从本地配置里读取AgileConfig的相关信息
-                var appId = localconfig["AgileConfig:appId"];
-                var secret = localconfig["AgileConfig:secret"];
-                var nodes = localconfig["AgileConfig:nodes"];
                 //new一个client实例
-                var configClient = new ConfigClient(appId, secret, nodes);
+                //使用无参构造函数会自动读取本地appsettings.json文件的AgileConfig节点的配置
+                var configClient = new ConfigClient();
                 //使用AddAgileConfig配置一个新的IConfigurationSource
                 config.AddAgileConfig(configClient);
                 //找一个变量挂载client实例，以便其他地方可以直接使用实例访问配置
                 ConfigClient = configClient;
                 //注册配置项修改事件
-                configClient.ConfigChanged += ConfigClient_ConfigChanged;
+                configClient.ConfigChanged += (arg) =>
+                {
+                    Console.WriteLine($"action:{arg.Action} key:{arg.Key}");
+                };
             })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
 
-        /// <summary>
-        /// 此事件会在配置项目发生新增、修改、删除的时候触发
-        /// </summary>
-        private static void ConfigClient_ConfigChanged(ConfigChangedArg obj)
-        {
-            Console.WriteLine($"action:{obj.Action} key:{obj.Key}");
-
-            switch (obj.Action)
-            {
-                case ActionConst.Add:
-                    break;
-                case ActionConst.Update:
-                    break;
-                case ActionConst.Remove:
-                    break;
-                default:
-                    break;
-            }
-        }
     }
 }
