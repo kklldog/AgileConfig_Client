eb85d3b25ddf2714cdfa94e2e2624d24f658617a
kklldog <kklldog@qq.com>
2021-04-18 17:29:21 +0800

    更新readme，sample等

--- AgileConfigWPFSample/App.xaml.cs
+++ AgileConfigWPFSample/App.xaml.cs
@@ -14,7 +14,7 @@ namespace AgileConfigWPFSample
     /// </summary>
     public partial class App : Application
     {
-        public static IConfigClient ConfigClient { get; private set; }
+        public static ConfigClient ConfigClient { get; private set; }
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             //跟控制台项目一样，appid等信息取决于你如何获取。你可以写死，可以从配置文件读取，可以从别的web service读取。
@@ -22,7 +22,8 @@ namespace AgileConfigWPFSample
             var secret = "";
             var nodes = "http://agileconfig.xbaby.xyz:5000";
             ConfigClient = new ConfigClient(appId, secret, nodes);
-
+            ConfigClient.Name = "wpfconfigclient";
+            ConfigClient.Tag = "t1";
             ConfigClient.ConnectAsync().GetAwaiter();
         }
     }
