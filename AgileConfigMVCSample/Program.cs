967e10525e0fcf894c3bb7e88e3b508c21085a51
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2021-08-18 10:08:06 +0800

    Update Program.cs

--- AgileConfigMVCSample/Program.cs
+++ AgileConfigMVCSample/Program.cs
@@ -18,12 +18,12 @@ namespace AgileConfigMVCSample
             Host.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration((context, config) =>
             {
-                //newһ��clientʵ��
-                //ʹ���޲ι��캯�����Զ���ȡ����appsettings.json�ļ���AgileConfig�ڵ������
+                //new一个client实例
+                //使用无参构造函数会自动读取本地appsettings.json文件的AgileConfig节点的配置
                 var configClient = new ConfigClient();
-                //ʹ��AddAgileConfig����һ���µ�IConfigurationSource
+                //使用AddAgileConfig配置一个新的IConfigurationSource
                 config.AddAgileConfig(configClient);
-                //ע���������޸��¼�
+                //注册配置项修改事件
                 configClient.ConfigChanged += (arg) =>
                 {
                     Console.WriteLine($"action:{arg.Action} key:{arg.Key}");
