1c66ba4e2f9e605f4e2e97c0e15acdd43ac03624
kklldog <kklldog@qq.com>
2021-09-23 00:27:12 +0800

    add net6 sample

---
+++ AgileConfigMVCSampleNET6/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((_, bd) => {
+    bd.AddAgileConfig(e => {
+        Console.WriteLine($"configs {e.Action}");
+    });
+});
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
